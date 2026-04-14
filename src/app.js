7406a033d3fe1ef598b12df6ee0c188208058091
Jhamilton <jhamiltonmunoz@gmail.com>
2026-04-14 11:21:56 -0500

    feat(api):implementa funcion healthcheck

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import { version } from "react";
+
 export function saludar(nombre){
 return "hola" + nombre + ", bienvendio coderider";
 }
@@ -17,4 +19,13 @@ mensaje: "sistema coderrider funcionando"
 
 export function sumar(a, b) {
     return a + b;
+}
+
+export function healthcheck(){
+    return {
+        status:'ok',
+        timestamp: new Date().toISOString(),
+        servicio: 'StreamFlow API',
+        version: '1.0.0'
+    };
 }
